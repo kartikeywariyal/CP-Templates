4fc942a668aec28d4777717f1e03b10ef4e3f97b
kartikeywariyal <kartikeywariyal706@gmail.com>
2025-08-01 12:29:03 +0530

    Create mainTemplate.cpp

---
+++ mainTemplate.cpp
@@ -0,0 +1,35 @@
+//█▄▀ ▄▀█ █▀█ ▀█▀ █ █▄▀ █▀▀ █▄█   █░█░█ ▄▀█ █▀█ █ █▄█ ▄▀█ █░░
+//█░█ █▀█ █▀▄ ░█░ █ █░█ ██▄ ░█░   ▀▄▀▄▀ █▀█ █▀▄ █ ░█░ █▀█ █▄▄
+/*======================== I Bow to Lord Shiva ======================*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define yes() cout << "YES" << endl
+#define no() cout << "NO" << endl
+#define all(arr) arr.begin(), arr.end()
+#define MOD 1000000007
+#define INF 1e18
+#define print1(a) cout << a << endl
+template < typename Container >
+    void print(const Container & container) {
+        for (const auto & element: container) {
+            cout << element << " ";
+        }
+        cout << endl;
+    }
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    solve();
+
+    return 0;
+}
